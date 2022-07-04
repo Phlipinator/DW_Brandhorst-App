937a4d0bf13e8d7a999ba07fd003548c9e5e7ecc
springscreen MULTIMEDIA <admin@springscreen.de>
2022-07-04 02:15:52 +0200

    improvements and added button click example

---
+++ Assets/UIComponents/InfoScreen.cs
@@ -0,0 +1,20 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class InfoScreen : EditorWindow
+{
+    [SerializeField]
+    private VisualTreeAsset m_VisualTreeAsset = default;
+
+    [MenuItem("Window/UI Toolkit/InfoScreen")]
+    public static void ShowExample()
+    {
+
+    }
+
+    public void CreateGUI()
+    {
+
+    }
+}
