1f05109c93c00350c826f18cfd75ee052c1e42ba
Agnes Reda <agnes@wreda.com>
2022-07-07 21:01:57 +0200

    Add function to buttons of the Menu Bar

--- Assets/UIComponents/MenuBar.cs
+++ Assets/UIComponents/MenuBar.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 using System.IO;
+using UnityEngine.SceneManagement;
 
 public class MenuBar : MonoBehaviour
 {
+    public Texture2D scannerIcon;
+    public SceneManagerScript sceneChange;
+
     void Awake()
     {
         //get Buttons of each Challenge
@@ -14,22 +18,36 @@ public class MenuBar : MonoBehaviour
         Button middle = root.Q<Button>("trophy");
         Button home = root.Q<Button>("home");
 
+        Debug.Log(SceneManager.GetActiveScene().buildIndex);
+
+        if (SceneManager.GetActiveScene().buildIndex >= 3)
+        {
+            middle.style.backgroundImage = new StyleBackground(scannerIcon);
+            middle.clicked += () =>
+            {
+                //Entweder Scanner öffnet sich oder man kommt zur Trophy Übersicht
+                Debug.Log("Middle");
+            };
+        }
+        else
+        {
+            middle.clicked += () =>
+            {
+                //Entweder Scanner öffnet sich oder man kommt zur Trophy Übersicht
+                Debug.Log("Middle");
+            };
+        }
+       
         back.clicked += () =>
         {
             //Szene zurück
             Debug.Log("zurück");
         };
 
-        middle.clicked += () =>
-        {
-            //Entweder Scanner öffnet sich oder man kommt zur Trophy Übersicht
-            Debug.Log("Middle");
-        };
-
         home.clicked += () =>
         {
             Debug.Log("Zurück zur Raumübersicht");
-            //man kommt zur RaumÜbersicht (Szene1_Room)
+            sceneChange.goToFirst();
         };
 
     }
