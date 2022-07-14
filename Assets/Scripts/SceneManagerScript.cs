10c3d0f347ff6fcff7914d39e38eacdccf13fc2b
Philipp Thalhammer <57066673+Phlipinator@users.noreply.github.com>
2022-07-14 16:31:47 +0200

    Added exeption to sceneManagerScript that permits QR-CodeScanner to be added to the sceneHistory

--- Assets/Scripts/SceneManagerScript.cs
+++ Assets/Scripts/SceneManagerScript.cs
@@ -15,7 +15,10 @@ public class SceneManagerScript : MonoBehaviour
     public void goToFirst()
     {
         // The next Line needs to be copied exactly like this to every new SceneSwitch-Function
-        PersistentManagerScript.Instance.addSceneToHistory(SceneManager.GetActiveScene().name);
+        if (SceneManager.GetActiveScene().name != "4_QR-Scanner")
+        {
+            PersistentManagerScript.Instance.addSceneToHistory(SceneManager.GetActiveScene().name);
+        }
         // The next line needs to be adapted with the new Scene name
         SceneManager.LoadScene("1_RoomOverview");
     }
@@ -36,7 +39,11 @@ public class SceneManagerScript : MonoBehaviour
     public void goToSecond(Room roomID)
     {
         PersistentManagerScript.Instance.roomID = roomID;
-        PersistentManagerScript.Instance.addSceneToHistory(SceneManager.GetActiveScene().name);
+        if (SceneManager.GetActiveScene().name != "4_QR-Scanner")
+        {
+            PersistentManagerScript.Instance.addSceneToHistory(SceneManager.GetActiveScene().name);
+        }
+        
         SceneManager.LoadScene("2_Challenges");
     }
 
@@ -44,27 +51,39 @@ public class SceneManagerScript : MonoBehaviour
     public void goToThird(int artworkID)
     {
         PersistentManagerScript.Instance.artworkID = artworkID;
-        PersistentManagerScript.Instance.addSceneToHistory(SceneManager.GetActiveScene().name);
+        if (SceneManager.GetActiveScene().name != "4_QR-Scanner")
+        {
+            PersistentManagerScript.Instance.addSceneToHistory(SceneManager.GetActiveScene().name);
+        }
         SceneManager.LoadScene("3_Detail-Challenges");
     }
 
     public void goToFourth()
     {
         PersistentManagerScript.Instance.doScanning = true;
-        PersistentManagerScript.Instance.addSceneToHistory(SceneManager.GetActiveScene().name);
+        if (SceneManager.GetActiveScene().name != "4_QR-Scanner")
+        {
+            PersistentManagerScript.Instance.addSceneToHistory(SceneManager.GetActiveScene().name);
+        }
         SceneManager.LoadScene("4_QR-Scanner");
     }
 
     public void goToFifth(int artID)
     {
         PersistentManagerScript.Instance.artworkID = artID;
-        PersistentManagerScript.Instance.addSceneToHistory(SceneManager.GetActiveScene().name);
+        if (SceneManager.GetActiveScene().name != "4_QR-Scanner")
+        {
+            PersistentManagerScript.Instance.addSceneToHistory(SceneManager.GetActiveScene().name);
+        }
         SceneManager.LoadScene("5_Info-Screens");
     }
 
     public void goToSixt()
     {
-        PersistentManagerScript.Instance.addSceneToHistory(SceneManager.GetActiveScene().name);
+        if (SceneManager.GetActiveScene().name != "4_QR-Scanner")
+        {
+            PersistentManagerScript.Instance.addSceneToHistory(SceneManager.GetActiveScene().name);
+        }
         SceneManager.LoadScene("6_Trophies");
     }
 
